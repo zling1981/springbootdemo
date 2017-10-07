d70a30c98e4672271e97ef83f4c23a0958b4e42f
zling1981 <6930124@qq.com>
2017-10-07 13:02:39 +0800

    hhh

--- src/main/java/com/victor/controller/SampleController.java
+++ src/main/java/com/victor/controller/SampleController.java
@@ -11,7 +11,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World!";
+        return "Hello World ddd!";
     }
 
     public static void main(String[] args) throws Exception {
