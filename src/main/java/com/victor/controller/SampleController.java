bf22be8615c1ecc719aa2f79f09df821149b9460
victor <6930124@qq.com>
2017-10-07 15:00:49 +0800

    jjjjj

--- src/main/java/com/victor/controller/SampleController.java
+++ src/main/java/com/victor/controller/SampleController.java
@@ -11,7 +11,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World cccccccccc!";
+        return "Hello World jjjjjjjjjjjjjj!";
     }
 
     public static void main(String[] args) throws Exception {
