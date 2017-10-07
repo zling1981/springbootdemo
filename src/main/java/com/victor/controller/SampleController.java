9bab5596ee4288b17444793a708cb120eea4f481
victor <6930124@qq.com>
2017-10-07 15:32:01 +0800

    test

--- src/main/java/com/victor/controller/SampleController.java
+++ src/main/java/com/victor/controller/SampleController.java
@@ -11,7 +11,7 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World branch88888888888888!";
+        return "Hello World branch88888888888888test!";
     }
 
     public static void main(String[] args) throws Exception {
